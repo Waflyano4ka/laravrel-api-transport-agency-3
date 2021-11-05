e0b8e232c46275b59364ccced30882fafc546dd7
Dworetskiy <vlad192002@gmail.com>
2021-11-05 21:33:00 +0300

    token 2

---
+++ database/migrations/2021_11_04_142751_create_office_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('office_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('office_id')->constrained('offices')->onUpdate('cascade')->onDelete('cascade');
+            $table->boolean('deleted')->default(false)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('office_users');
+    }
+}
