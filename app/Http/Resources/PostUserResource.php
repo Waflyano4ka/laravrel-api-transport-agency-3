d638dcd498ba125c6f7bdf6609c38baadb737ac9
Dworetskiy <vlad192002@gmail.com>
2021-11-07 00:00:59 +0300

    foundation table 2

---
+++ app/Http/Resources/PostUserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user' => new UserResource($this->user),
+            'post' => new PostResource($this->post),
+        ];
+    }
+}
